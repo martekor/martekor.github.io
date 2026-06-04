e63156be80c844d420f13e2f79c0e8c06110e3b4
martekor <agenticflow.en@gmail.com>
2026-06-04 13:00:25 +0100

    Update register.js

--- register.js
+++ register.js
@@ -4,7 +4,7 @@ document.getElementById('registerForm').addEventListener('submit', function(e) {
         username: this.querySelector('[name="username"]').value,
         password: this.querySelector('[name="password"]').value
     };
-    fetch('http://localhost:5000/register', {
+    fetch('https://svirityofficiel2.pythonanywhere.com/register', {
         method: 'POST',
         headers: {'Content-Type': 'application/json'},
         body: JSON.stringify(data)
